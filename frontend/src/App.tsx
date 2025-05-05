df5ee583d725f7e8e3857f3dc299dd3857ac4cc3
Jumpi96 <juampilorenzo@gmail.com>
2025-05-05 08:50:52 +0200

    Revert "try wardrobe"
    This reverts commit 1d900eee24a39fcca0e535be0b5b06a3274499f9.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
-import WardrobePage from './pages/WardrobePage'
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        {/* Navigation */}
-        <nav className="bg-white shadow-sm">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16">
-              <div className="flex space-x-8">
-                <Link to="/" className="inline-flex items-center px-1 pt-1 text-gray-900 hover:text-gray-600">
-                  Home
-                </Link>
-                <Link to="/wardrobe" className="inline-flex items-center px-1 pt-1 text-gray-900 hover:text-gray-600">
-                  Wardrobe
-                </Link>
-              </div>
-            </div>
-          </div>
-        </nav>
-
-        {/* Routes */}
-        <main className="py-10">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/wardrobe" element={<WardrobePage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <Home />
   )
 }
 
