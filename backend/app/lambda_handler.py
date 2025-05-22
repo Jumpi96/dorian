deb4346f61e30680b0545cf8de93a74396d68d53
Jumpi96 <juampilorenzo@gmail.com>
2025-05-22 19:53:35 +0200

    removing debugging

--- backend/app/lambda_handler.py
+++ backend/app/lambda_handler.py
@@ -1,13 +1,7 @@
 from app import create_app
 import awsgi
-import json
-import logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 app = create_app()
 
 def lambda_handler(event, context):
-    logger.info(f"Received event: {json.dumps(event)}")
     return awsgi.response(app, event, context) 
