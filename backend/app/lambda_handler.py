169c152354c0c52dd422d063d374dab6ac0298df
Jumpi96 <juampilorenzo@gmail.com>
2025-05-22 19:46:23 +0200

    adding debugging

--- backend/app/lambda_handler.py
+++ backend/app/lambda_handler.py
@@ -1,7 +1,13 @@
 from app import create_app
 import awsgi
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 app = create_app()
 
 def lambda_handler(event, context):
+    logger.info(f"Received event: {json.dumps(event)}")
     return awsgi.response(app, event, context) 
