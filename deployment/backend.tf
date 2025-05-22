2651a598be76dc87752c220f3e1639955f891f28
Jumpi96 <juampilorenzo@gmail.com>
2025-05-22 18:20:31 +0200

    fmt

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "dorian-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
+    bucket  = "dorian-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
   }
 } 
